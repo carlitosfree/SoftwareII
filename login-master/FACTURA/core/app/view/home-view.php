de72af642baef72fb046edffedd86d2766a67604
RONALD STALIN CARGUA VILANA <ronald.cargua@epn.edu.ec>
2018-08-09 06:44:11 -0500

    final

--- login-master/FACTURA/core/app/view/home-view.php
+++ login-master/FACTURA/core/app/view/home-view.php
@@ -11,17 +11,17 @@
 ?>
 <div class="row">
 	<div class="col-md-12">
-		<h1>The Exotic Rose</h1>
+		<h1>RoseSoft</h1>
 </div>
 </div>
   <div class="row">
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-aqua">
+          <div class="small-box bg-green">
             <div class="inner">
               <h3>
 			  <!--?php echo count(ProductData::getAll());?-->
-			  <i class="fa fa-book fa-3x"> </i>
+			  <i class="fa fa-glass fa-3x"> </i>
 			  </h3>
 
               <p>Productos</p>
@@ -35,7 +35,7 @@
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-purple">
+          <div class="small-box bg-primary">
             <div class="inner">
               <h3>
 			  <!--?php echo count(PersonData::getClients());?-->
@@ -51,10 +51,28 @@
             <a href="./?view=clients" class="small-box-footer">Ver mas <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
-		 <!-- ./col -->
+		<!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-purple">
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>
+			  <!--?php echo count(CategoryData::getAll());?-->
+			  <i class="fa fa-shopping-cart fa-3x"> </i>
+			  </h3>
+
+              <p>Ventas</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="./?view=sellreports" class="small-box-footer">Ver mas <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+		<!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
             <div class="inner">
               <h3>
 			  <!--?php echo count(PersonData::getProviders());?-->
@@ -67,24 +85,6 @@
               <i class="ion ion-stats-bars"></i>
             </div>
             <a href="./?view=provider" class="small-box-footer">Ver mas <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-red">
-            <div class="inner">
-              <h3>
-			  <!--?php echo count(CategoryData::getAll());?-->
-			  <i class="fa fa-book fa-3x"> </i>
-			  </h3>
-
-              <p>Categorias</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
-            </div>
-            <a href="./?view=categories" class="small-box-footer">Ver mas <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
